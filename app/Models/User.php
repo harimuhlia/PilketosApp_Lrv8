ac56604cf81f991fe77653d1f27c27fbbbe89adc
harimuhlia <harimuhlia@gmail.com>
2023-06-10 14:39:36 +0700

    Fix Roles Pemilih Pada Edit/ Update

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'nis', 'kelas', 'role', 'status',
+        'nis', 'kelas', 'roles', 'status',
     ];
 
     /**
