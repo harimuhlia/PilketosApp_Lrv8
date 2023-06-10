248cca0ea0fc2d07abb23034f8471c7a41095336
harimuhlia <harimuhlia@gmail.com>
2023-06-10 14:31:56 +0700

    Perbaikan Halaman Login/ Merubah tampilan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,115 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Pilketos | Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="{{ asset('Template')}}/loginform/css/style.css">
+
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+            @if($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+			<div class="row justify-content-center">
+				<div class="col-md-12 col-lg-10">
+					<div class="wrap d-md-flex">
+                      
+						<div class="text-wrap p-4 p-lg-5 text-center d-flex align-items-center order-md-last">
+							<div class="text w-100">
+								<h2>Selamat Datang!</h2>
+								<p>Di Aplikasi Pemilihan Ketua OSIS (PilketosApp) SMK Negeri 5 Kabupaten Tangerang</p>
+                                
+								<a href="#" class="btn btn-white btn-outline-white">Daftar</a>
+							</div>
+			      </div>
+						<div class="login-wrap p-4 p-lg-5">
+			      	<div class="d-flex">
+			      		<div class="w-100">
+			      			<h3 class="mb-4">Silakan LOGIN</h3>
+			      		</div>
+								<div class="w-100">
+									<p class="social-media d-flex justify-content-end">
+										<a href="https://smkn5kabtangerangmauk.sch.id/" target="_blank" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-globe"></span></a>
+										<a href="https://www.instagram.com/smkn5kabtang/" target="_blank" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-instagram"></span></a>
+									</p>
+								</div>
+			      	</div>
+						<form method="POST" action="{{ route('login') }}" class="signin-form">
+                            @csrf
+			      		{{-- <div class="form-group mb-3">
+			      			<label class="label" for="name">Email</label>
+			      			<input type="text" class="form-control" name="email" placeholder="Email" required>
+			      		</div>
+                          @error('email')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                        @enderror --}}
+                        <div class="form-group mb-3">
+                            <label class="label" for="name">NIS Sekolah</label>
+                            <input id="nis" type="text" class="form-control" name="nis" placeholder="Masukan NIS Sekolah" required>
+                        </div>
+                        @error('nis')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                      @enderror
+		            <div class="form-group mb-3">
+		            	<label class="label" for="password">Password</label>
+		              <input type="password" class="form-control" name="password" placeholder="Masukan Password" required>
+		            </div>
+                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+		            <div class="form-group">
+		            	<button type="submit" class="form-control btn btn-primary submit px-3">Masuk</button>
+		            </div>
+		            {{-- <div class="form-group d-md-flex">
+		            	<div class="w-50 text-left">
+			            	<label class="checkbox-wrap checkbox-primary mb-0">Ingat Saya
+									  <input type="checkbox" checked>
+									  <span class="checkmark"></span>
+										</label>
+									</div>
+									<div class="w-50 text-md-right">
+										<a href="#">Lupa Password</a>
+									</div>
+		            </div> --}}
+		          </form>
+		        </div>
+		      </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+    <script src="{{ asset('Template')}}/loginform/js/jquery.min.js"></script>
+    <script src="{{ asset('Template')}}/loginform/js/popper.js"></script>
+    <script src="{{ asset('Template')}}/loginform/js/bootstrap.min.js"></script>
+    <script src="{{ asset('Template')}}/loginform/js/main.js"></script>
+
+	</body>
+</html>
+
+
+
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -70,4 +181,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
