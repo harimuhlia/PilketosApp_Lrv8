08314d241b5ef4b3205913d970bffe589a9777d2
harimuhlia <harimuhlia@gmail.com>
2023-08-30 14:24:17 +0700

    Update halaman votting

--- resources/views/tampilan/footer.blade.php
+++ resources/views/tampilan/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      Anything you want
+      Created By HM
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2023 | PilketosApp SMKN 5 Kab. Tangerang | Template From <a href="https://wa.me/6287779533395" target="_blank">AdminLTE.io</a>.</strong> All rights reserved.
   </footer>
